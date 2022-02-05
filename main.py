d00eae09e96ebfe05a39d882e6af5a3f49d9b550
Thomas Höß <mail@thomsn.de>
2022-02-05 17:08:17 +0100

    Create main.py
    Initial commit

---
+++ main.py
@@ -0,0 +1,133 @@
+import os
+import time
+import requests
+from datetime import datetime
+import requests_cache
+from mutagen.mp4 import MP4
+
+
+# Enable caching of API requests/responses for 24h
+requests_cache.install_cache(cache_name='mgtv_cache', backend='sqlite', expire_after=86400)
+
+
+def api_call(params, username, password):
+    # The API has a rate limiter, this while loop will retry if the API spits out an error because of an exceeded rate limit
+    while True:
+        response = requests.get('https://' + username + ':' + password + '@massengeschmack.tv/api/v1/', params=params)
+        response = response.json()
+
+        # Check if retryAfter is set in response of API (attr only exists if rate limited)
+        try:
+            response["retryAfter"]
+        except KeyError:
+            response["retryAfter"] = None
+
+        # If rate limit reached, wait the needed time and start the loop over again
+        if response["retryAfter"] is not None:
+            retry = response["retryAfter"]
+            time.sleep(retry)
+            continue
+        else:
+            break
+
+    return response
+
+
+def list_formats():
+    # List all formats and their ids
+    formats = []
+
+    i = 0
+    while True:
+        params = (
+            ("action", "getFeed"),
+            ('from', "[" + str(i) + "]"),
+            ('limit', "1")
+        )
+        # Store number of pages (API does pagination)
+        response = api_call(params, username, password)
+
+        # Check if format exists / has at least one video, break loop when last format is reached
+        if response["pages"] > 0:
+            formats.append(str(response["eps"][0]["pid"]) + ": " + response["eps"][0]["pdesc"])
+            i = i + 1
+        else:
+            break
+
+    return formats
+
+
+def create_list(pid):
+    # Create empty array to store ids of videos to be downloaded
+    downloadIds = []
+
+    # Get list of all episodes of specified format via API
+    params = (
+        ("action", "getFeed"),
+        ('from', "[" + str(pid) + "]"),
+        ('page', "1")
+    )
+
+    # Store number of pages (API does pagination)
+    pages = api_call(params, username, password)["pages"]
+
+    # Cycle through all pages of the specified format and append the video identifier in the array
+    for i in range(0, pages):
+        params = (
+            ("action", "getFeed"),
+            ('from', "[" + str(pid) + "]"),
+            ('page', i)
+        )
+        page = api_call(params, username, password)
+        for v in page["eps"]:
+            downloadIds.append(v["identifier"])
+
+    return downloadIds
+
+
+def dl_video(id, path):
+    # Get clip information from API
+    params = (
+        ('action', "getClip"),
+        ('identifier', id),
+    )
+    response = api_call(params, username, password)
+
+    # Format various information
+    fileType = "." + response["files"][0]["url"].split(".")[-1]
+    fileName = response["title"] + " - " + datetime.utcfromtimestamp(response["date"]).strftime('%Y-%m-%d')
+
+    # Download the video via system-wide install of yt-dlp
+    ytdlpCmd = 'yt-dlp https:' + response["files"][0]["url"] + ' -o "' + fileName + fileType + '" -P "' + path
+    os.system(ytdlpCmd)
+
+    # Modify metadata after download
+    video = MP4(path + fileName + fileType)
+    video["desc"] = response["desc"]
+    video["date"] = datetime.utcfromtimestamp(response["date"]).strftime('%Y-%m-%d')
+    video.save()
+
+# Get username and password to have access to download videos
+username = input("E-Mail Adresse: ")
+username = username.replace("@", "--at--")
+
+password = input("Passwort: ")
+
+# Ask for download destination and add trailing slash
+path = input("Download-Pfad: ")
+path = os.path.join(path, '')
+
+# List all available formats and their pid
+print("Formate und deren ID:")
+for i in list_formats():
+    print(i)
+
+# Ask for program id
+pid = input("ID des Formats: ")
+
+# Create list of series
+list = create_list(pid)
+
+# Cycle through episodes and download videos
+for i in list:
+    dl_video(i, path)
